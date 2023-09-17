2826647adc5ef0ff8d77897846003848e5a2b689
artem <barishartem@gmail.com>
2023-09-17 22:09:50 +0300

    Added file of automated testing

---
+++ src/main/java/com/example/airtrip/exception/PlaneHasTripException.java
@@ -0,0 +1,7 @@
+package com.example.airtrip.exception;
+
+public class PlaneHasTripException extends RuntimeException {
+    public PlaneHasTripException(String message) {
+        super(message);
+    }
+}
